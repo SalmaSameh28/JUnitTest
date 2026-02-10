4179ca954ef84d8e32c05dfe0a4c1d2c0b291af3
SalmaSameh28 <salmasamehabdulmohsen@gmail.com>
2026-02-10 18:06:58 +0200

    comment in file

--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -3,7 +3,7 @@ package org.example.service;
 import org.example.model.User;
 
 public class UserService {
-
+    /// fsdfasdf
     public boolean isAdult(User user){return user.getAge()>=10;}
 
     public String greet (User user){
